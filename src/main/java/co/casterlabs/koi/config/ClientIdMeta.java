1aa4ccfa5eae1615bd8b57c47a7d6573806959c4
Ender <33337309+e3ndr@users.noreply.github.com>
2021-03-07 15:57:08 -0600

    Fix the UNKNOWN client id.

--- src/main/java/co/casterlabs/koi/config/ClientIdMeta.java
+++ src/main/java/co/casterlabs/koi/config/ClientIdMeta.java
@@ -2,13 +2,15 @@ package co.casterlabs.koi.config;
 
 import com.google.gson.annotations.SerializedName;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@AllArgsConstructor
 public class ClientIdMeta {
-    public static final ClientIdMeta UNKNOWN = new ClientIdMeta();
+    public static final ClientIdMeta UNKNOWN = new ClientIdMeta(false, true, "UNKNOWN");
 
     @SerializedName("non_logging")
     private boolean nonLogging = false;
