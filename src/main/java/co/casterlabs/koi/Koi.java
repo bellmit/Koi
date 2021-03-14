db85c65d9945e9477448b1c3315e163891a8a13b
Ender <33337309+e3ndr@users.noreply.github.com>
2021-03-14 17:11:49 -0500

    2.21.0

--- src/main/java/co/casterlabs/koi/Koi.java
+++ src/main/java/co/casterlabs/koi/Koi.java
@@ -55,7 +55,7 @@ public class Koi {
             .create();
     //@formatter:on
 
-    public static final String VERSION = "2.20.4";
+    public static final String VERSION = "2.21.0";
 
     private static @Getter ThreadPoolExecutor eventThreadPool = new ThreadPoolExecutor(16, 128, 480, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
     private static @Getter ThreadPoolExecutor clientThreadPool = new ThreadPoolExecutor(4, 16, 30, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
