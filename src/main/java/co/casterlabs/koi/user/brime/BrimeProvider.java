b38f1c0297ed74085b45e635fa76beaa26ad9ba5
Ender <33337309+e3ndr@users.noreply.github.com>
2021-03-21 15:14:47 -0500

    Add support for viewers and stream status on Brime (2.26.0)

--- src/main/java/co/casterlabs/koi/user/brime/BrimeProvider.java
+++ src/main/java/co/casterlabs/koi/user/brime/BrimeProvider.java
@@ -1,17 +1,24 @@
 package co.casterlabs.koi.user.brime;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import co.casterlabs.apiutil.auth.ApiAuthException;
 import co.casterlabs.apiutil.web.ApiException;
 import co.casterlabs.brimeapijava.realtime.BrimeRealtime;
 import co.casterlabs.brimeapijava.requests.BrimeGetChannelFollowerCountRequest;
+import co.casterlabs.brimeapijava.requests.BrimeGetChannelRequest;
+import co.casterlabs.brimeapijava.requests.BrimeGetStreamsRequest;
 import co.casterlabs.brimeapijava.requests.BrimeSendChatMessageRequest;
+import co.casterlabs.brimeapijava.types.BrimeChannel;
+import co.casterlabs.brimeapijava.types.BrimeStream;
 import co.casterlabs.koi.Koi;
 import co.casterlabs.koi.RepeatingThread;
 import co.casterlabs.koi.client.Client;
 import co.casterlabs.koi.client.ClientAuthProvider;
 import co.casterlabs.koi.client.ConnectionHolder;
+import co.casterlabs.koi.events.StreamStatusEvent;
 import co.casterlabs.koi.events.UserUpdateEvent;
 import co.casterlabs.koi.user.IdentifierException;
 import co.casterlabs.koi.user.User;
@@ -37,8 +44,9 @@ public class BrimeProvider implements UserProvider {
             client.setProfile(asUser);
             client.setSimpleProfile(asUser.getSimpleProfile());
 
-            client.getConnections().add(getChatConnection(client, brimeAuth));
+            client.getConnections().add(getRealtimeConnection(client, brimeAuth));
             client.getConnections().add(getProfileUpdater(client, brimeAuth));
+            client.getConnections().add(getStreamPoller(client, brimeAuth.getUsername()));
 
             client.broadcastEvent(new UserUpdateEvent(asUser));
         } catch (Exception e) {
@@ -48,7 +56,18 @@ public class BrimeProvider implements UserProvider {
 
     @Override
     public void hook(@NonNull Client client, @NonNull String username) throws IdentifierException {
-        throw new IdentifierException();
+        try {
+            User asUser = BrimeUserConverter.getInstance().get(username);
+
+            client.setProfile(asUser);
+            client.setSimpleProfile(asUser.getSimpleProfile());
+
+            client.getConnections().add(getStreamPoller(client, username));
+
+            client.broadcastEvent(new UserUpdateEvent(asUser));
+        } catch (Exception e) {
+            throw new IdentifierException();
+        }
     }
 
     @Override
@@ -72,7 +91,7 @@ public class BrimeProvider implements UserProvider {
     }
 
     @SuppressWarnings("deprecation")
-    private static ConnectionHolder getChatConnection(Client client, BrimeUserAuth brimeAuth) throws AblyException {
+    private static ConnectionHolder getRealtimeConnection(Client client, BrimeUserAuth brimeAuth) throws AblyException {
         String key = brimeAuth.getUUID() + ":realtime";
 
         ConnectionHolder holder = (ConnectionHolder) connectionCache.getItemById(key);
@@ -114,6 +133,71 @@ public class BrimeProvider implements UserProvider {
             });
 
             holder = new ConnectionHolder(key, client.getSimpleProfile());
+
+            holder.getClients().add(client);
+
+            holder.setCloseable(thread);
+
+            thread.start();
+
+            connectionCache.registerItem(key, holder);
+        } else {
+            holder.getClients().add(client);
+        }
+
+        return holder;
+    }
+
+    private static ConnectionHolder getStreamPoller(Client client, String username) {
+        String key = username + ":stream";
+
+        ConnectionHolder holder = (ConnectionHolder) connectionCache.getItemById(key);
+
+        if (holder == null) {
+            holder = new ConnectionHolder(key, client.getSimpleProfile());
+
+            ConnectionHolder pointer = holder;
+
+            RepeatingThread thread = new RepeatingThread("Brime stream poller " + username, TimeUnit.MINUTES.toMillis(1), new Runnable() {
+                private Instant streamStartedAt;
+
+                private String title = "";
+
+                @Override
+                public void run() {
+                    try {
+                        List<BrimeStream> streams = new BrimeGetStreamsRequest().send();
+
+                        boolean isLive = false;
+
+                        for (BrimeStream stream : streams) {
+                            if (stream.getStreamer().equalsIgnoreCase(username)) {
+                                BrimeChannel channel = new BrimeGetChannelRequest(username).send();
+
+                                isLive = true;
+                                this.title = channel.getTitle();
+
+                                break;
+                            }
+                        }
+
+                        if (isLive) {
+                            if (this.streamStartedAt == null) {
+                                this.streamStartedAt = Instant.now();
+                            }
+                        } else {
+                            this.streamStartedAt = null;
+                        }
+
+                        StreamStatusEvent e = new StreamStatusEvent(isLive, this.title, pointer.getProfile(), this.streamStartedAt);
+
+                        pointer.broadcastEvent(e);
+                        pointer.setHeldEvent(e);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
 
             holder.getClients().add(client);
 
--- src/main/java/co/casterlabs/koi/user/brime/BrimeRealtimeAdapter.java
+++ src/main/java/co/casterlabs/koi/user/brime/BrimeRealtimeAdapter.java
@@ -1,20 +1,32 @@
 package co.casterlabs.koi.user.brime;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 import co.casterlabs.brimeapijava.realtime.BrimeRealtime;
 import co.casterlabs.brimeapijava.realtime.BrimeRealtimeListener;
 import co.casterlabs.koi.Koi;
 import co.casterlabs.koi.client.ConnectionHolder;
 import co.casterlabs.koi.events.ChatEvent;
 import co.casterlabs.koi.events.FollowEvent;
+import co.casterlabs.koi.events.ViewerJoinEvent;
+import co.casterlabs.koi.events.ViewerLeaveEvent;
+import co.casterlabs.koi.events.ViewerListEvent;
 import co.casterlabs.koi.user.User;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import xyz.e3ndr.fastloggingframework.logging.FastLogger;
 import xyz.e3ndr.fastloggingframework.logging.LogLevel;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class BrimeRealtimeAdapter implements BrimeRealtimeListener {
-    private ConnectionHolder holder;
-    private BrimeRealtime conn;
+    private final ConnectionHolder holder;
+    private final BrimeRealtime conn;
+
+    private List<String> viewers = new LinkedList<>();
+    private Set<String> oldViewersSet = new HashSet<>();
 
     @Override
     public void onChat(String username, String color, String message) {
@@ -35,7 +47,56 @@ public class BrimeRealtimeAdapter implements BrimeRealtimeListener {
     }
 
     @Override
+    public void onJoin(String username) {
+        this.viewers.add(username);
+
+        this.updateViewers();
+    }
+
+    @Override
+    public void onLeave(String username) {
+        this.viewers.remove(username);
+
+        this.updateViewers();
+    }
+
+    private synchronized void updateViewers() {
+        Set<String> set = new HashSet<>(this.viewers);
+        List<User> viewers = new ArrayList<>();
+
+        for (String username : set) {
+            User viewer = BrimeUserConverter.getInstance().get(username);
+
+            viewers.add(viewer);
+
+            // JOIN
+            if (!this.oldViewersSet.contains(username)) {
+                this.holder.broadcastEvent(new ViewerJoinEvent(viewer, this.holder.getProfile()));
+            }
+        }
+
+        for (String username : this.oldViewersSet) {
+            // LEAVE
+            if (!set.contains(username)) {
+                User viewer = BrimeUserConverter.getInstance().get(username);
+
+                this.holder.broadcastEvent(new ViewerLeaveEvent(viewer, this.holder.getProfile()));
+            }
+        }
+
+        this.oldViewersSet = set;
+
+        ViewerListEvent event = new ViewerListEvent(viewers, this.holder.getProfile());
+
+        this.holder.setHeldEvent(event);
+        this.holder.broadcastEvent(event);
+    }
+
+    @Override
     public void onClose() {
+        this.viewers.clear();
+        this.oldViewersSet.clear();
+
         if (!this.holder.isExpired()) {
             Koi.getClientThreadPool().submit(() -> this.conn.connect());
         } else {
