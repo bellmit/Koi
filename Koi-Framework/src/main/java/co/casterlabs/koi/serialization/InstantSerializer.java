79af5dc95cbe6ede6ea57f49945537953d9a94e2
Ender <33337309+e3ndr@users.noreply.github.com>
2021-11-27 16:44:36 -0600

    Framework: Add missing GSON deserializer for java.lang.Instant
    This fixes Twitch Integration.

--- Koi-Framework/src/main/java/co/casterlabs/koi/serialization/InstantSerializer.java
+++ Koi-Framework/src/main/java/co/casterlabs/koi/serialization/InstantSerializer.java
@@ -3,16 +3,24 @@ package co.casterlabs.koi.serialization;
 import java.lang.reflect.Type;
 import java.time.Instant;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
-public class InstantSerializer implements JsonSerializer<Instant> {
+public class InstantSerializer implements JsonSerializer<Instant>, JsonDeserializer<Instant> {
 
     @Override
     public JsonElement serialize(Instant src, Type typeOfSrc, JsonSerializationContext context) {
         return new JsonPrimitive(src.toString());
+    }
+
+    @Override
+    public Instant deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        return Instant.parse(json.getAsString());
     }
 
 }
